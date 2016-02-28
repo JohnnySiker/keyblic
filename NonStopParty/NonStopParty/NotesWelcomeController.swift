1ae8e7e667eaf22cd42aa313d7f394d40a420664
Jonathan Velazquez <jonhysiker@icloud.com>
2016-02-28 03:36:14 -0600

    Welcome view and login view

---
+++ NonStopParty/NonStopParty/NotesWelcomeController.swift
@@ -0,0 +1,25 @@
+//
+//  NotesWelcomeController.swift
+//  NonStopParty
+//
+//  Created by Jonathan Velazquez on 28/02/16.
+//  Copyright © 2016 Keyblic. All rights reserved.
+//
+
+import UIKit
+
+class NotesWelcomeController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
