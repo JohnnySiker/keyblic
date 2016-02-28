61c1ee597a40a9dd86407f71ff59ed4d1ec28d2e
L Daniel De San Pedro V <ldanieldesanpedro@gmail.com>
2016-02-28 09:42:01 -0600

    Avancé en la pantalla de eventos

---
+++ NonStopParty/NewEventViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewEventViewController.swift
+//  
+//
+//  Created by Luis  Daniel De San Pedro on 28/02/16.
+//
+//
+
+import UIKit
+
+class NewEventViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
