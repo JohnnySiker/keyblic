33efcd1c43bec6ba32ee2b2c51d06b2641a79912
L Daniel De San Pedro V <ldanieldesanpedro@gmail.com>
2016-02-28 00:36:15 -0600

    Añadí la Bridging-Header.h

---
+++ NonStopParty/NonStopParty/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Briging-Header.h
+//  NonStopParty
+//
+//  Created by Luis  Daniel De San Pedro on 27/02/16.
+//  Copyright © 2016 Keyblic. All rights reserved.
+//
+
+#ifndef Briging_Header_h
+#define Briging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif /* Briging_Header_h */
